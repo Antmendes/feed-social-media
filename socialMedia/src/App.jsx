833a79841605a982ed93c22af71c7801ba80ecbf
Antmendes <antoniomendesjunior02@gmail.com>
2023-08-09 17:58:39 -0300

    usando o useState pra fazer os comentarios e posts dinamicos

--- socialMedia/src/App.jsx
+++ socialMedia/src/App.jsx
@@ -5,6 +5,39 @@ import { Sidebar } from './components/Sidebar'
 import './global.css'
 import styles from './App.module.css'
 
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/Antmendes.png',
+      name: 'Antonio Mendes',
+      role: 'Web Developer'
+    },
+    content:[
+      { type: 'paragraph', content: 'Fala galera!!!'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portfólio. é um projeto que fiz no curso ignite. o nome do projeto é socialMedia/feed'},
+      { type: 'link', content: 'social.media/feed'}
+    ],
+    publishedAt: new Date('2023-08-09 16:13:00')
+  }, 
+   {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/Antmendes.png',
+      name: 'José Silva',
+      role: 'Web Developer'
+    },
+    content:[
+      { type: 'paragraph', content: 'Fala galera!!!'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portfólio. é um projeto que fiz no curso ignite. o nome do projeto é socialMedia/feed'},
+      { type: 'link', content: 'social.media/feed'}
+    ],
+    publishedAt: new Date('2023-08-08 22:30:00')
+  }
+]
+
+
 export function App() {
   
 
@@ -15,10 +48,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-           author="Antonio Mendes"  
-           content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quas quis, amet doloribus inventore labore natus sed. Cumque, minima molestiae nihil voluptatibus nisi, ut necessitatibus placeat laborum provident, veritatis tempora." 
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
       
--- socialMedia/src/components/Post.jsx
+++ socialMedia/src/components/Post.jsx
@@ -1,44 +1,88 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
+import { useState } from 'react'
 
-export function Post() {
+
+export function Post({ author, publishedAt, content }) {
+  const [comments, setComments]= useState(['Post muito bacana, hein?!'])
+
+  const [newCommentText, setNewCommentText] = useState('')
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+
+    setComments([...comments, newCommentText])
+    setNewCommentText('')
+
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
+  }
+
     return (
         <article className={styles.post}> 
           <header>
             <div className={styles.author}>
-                <Avatar  src="https://github.com/Antmendes.png" />
+                <Avatar  src={author.avatarUrl}/>
                 <div className={styles.authorInfo}>
-                    <strong>Antonio Mendes</strong>
-                    <span>Web Developer</span>
+                    <strong>{author.name}</strong>
+                    <span>{author.role}</span>
                 </div>
             </div>
 
-            <time title='08 de agosto às 18:50' dateTime='2023-08-08 18:50:30'>publicado há 1h</time>
+            <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+              {publishedDateRelativeToNow}
+            </time>
           </header>
 
           <div className={styles.content}>
-            <p>Fala galera!!</p>
-            <p>Acabei de subir mais um projeto no meu portfólio. é um projeto que fiz no curso ignite. o nome do projeto é socialMedia/feed </p>
-            <p><a href="">social.media/feed</a></p>
+          {content.map(line => {
+              if (line.type === 'paragraph') {
+                return <p>{line.content}</p>
+              } else if (line.type === 'link') {
+                return <p><a href="#">{line.content}</a></p>
+              }
+            })}
+            
+            
             <p>
                 <a href="">#novoprojeto</a>{' '}
                 <a href="">#react</a>
-                </p>
+              </p>
           </div>
 
-          <form className={styles.commentForm}>
+          <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
             <strong>Deixe seu feedback</strong>
-            <textarea placeholder='Deixe um comentário'/>
+            <textarea 
+               name="comment"
+               placeholder='Deixe um comentário'
+               value={newCommentText}
+               onChange={handleNewCommentChange}
+            />
             <footer>
               <button type='submit'>Publicar</button>
             </footer>
           </form>
 
           <div className={styles.commentList}>
-            <Comment />
-            <Comment />
-            <Comment />
+            {comments.map(comment => {
+              return <Comment content={comment} />
+            })}
+            
           </div>
         </article>
     )
